3b9200a1d6c1e32441fa79aac2ff678524c5fe0b
jrivas112 <147008410+jrivas112@users.noreply.github.com>
2024-08-10 20:06:22 -0700

    test
    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // hr You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
     
